4458e8e6f01d792e03b6a92a734b4d1347796f49
nehemie-n <nehemie054@gmail.com>
2023-09-06 18:36:59 +0200

    feat: added audio

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,18 @@ use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // import audios
-    let audio = Audio::new();
-    // audio.add("name", path);
+    let mut audio = Audio::new();
+
+    audio.add("explode", "audio/explode.wav");
+    audio.add("lose", "audio/lose.wav");
+    audio.add("move", "audio/move.wav");
+    audio.add("pew", "audio/pew.wav");
+    audio.add("startup", "audio/startup.wav");
+    audio.add("win", "audio/win.wav");
+
+    audio.play("startup");
+    // wait
+    audio.wait();
 
     return Ok(());
 }
