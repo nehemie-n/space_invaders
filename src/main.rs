93459a3e9b9d4ba730b5f1b7dab12e01f2f84630
nehemie-n <nehemie054@gmail.com>
2023-09-06 18:51:38 +0200

    feat: put sound names in an enum

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,39 @@ use std::error::Error;
 
 use rusty_audio::Audio;
 
+enum Sounds {
+    EXPLODE,
+    LOSE,
+    MOVE,
+    PEW,
+    STARTUP,
+    WIN,
+}
+impl Sounds {
+    fn name(&self) -> String {
+        return match &self {
+            Sounds::EXPLODE => String::from("explode"),
+            Sounds::LOSE => String::from("lose"),
+            Sounds::MOVE => String::from("move"),
+            Sounds::PEW => String::from("pew"),
+            Sounds::STARTUP => String::from("startup"),
+            Sounds::WIN => String::from("win"),
+        };
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // import audios
     let mut audio = Audio::new();
 
-    audio.add("explode", "audio/explode.wav");
-    audio.add("lose", "audio/lose.wav");
-    audio.add("move", "audio/move.wav");
-    audio.add("pew", "audio/pew.wav");
-    audio.add("startup", "audio/startup.wav");
-    audio.add("win", "audio/win.wav");
+    audio.add(Sounds::EXPLODE.name(), "audio/explode.wav");
+    audio.add(Sounds::LOSE.name(), "audio/lose.wav");
+    audio.add(Sounds::MOVE.name(), "audio/move.wav");
+    audio.add(Sounds::PEW.name(), "audio/pew.wav");
+    audio.add(Sounds::STARTUP.name(), "audio/startup.wav");
+    audio.add(Sounds::WIN.name(), "audio/win.wav");
 
-    audio.play("startup");
+    audio.play(Sounds::LOSE.name());
     // wait
     audio.wait();
 
